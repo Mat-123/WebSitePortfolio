f95199548414e02c45f50e984ca8943a4701ef5e
Matteo <156803363+Mat-123@users.noreply.github.com>
2024-12-07 15:31:45 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,8 @@ export const dialogueData = {
     pc: '<ul style="list-style-type: none;"><li>Accesso in corso...</li><li>Accesso effettuato.</li></ul>Benvenuto! Ecco i link a <a href="https://github.com/Mat-123" target="_blank">GitHub</a> e al profilo <a href="https://www.linkedin.com/in/matteo-montani-36a0a02b9/" target="_blank">LinkedIn</a>.',
     library: `Sono una persona molto curiosa e mi piace conoscere cose nuove, ecco gli strumenti che utilizzo:
     <ul>
+    <li>C#</li>
+    <li>.NET</li>
     <li>Javascript</li>
     <li>PHP</li>
     <li>MySQL</li>
@@ -14,7 +16,10 @@ export const dialogueData = {
     bed: `Letto? Riposo? Come Dev non conosco questi linguaggi...`,
     cv: `Sul tavolo c'è un foglio, è il mio Curriculum. <a href="https://github.com/Mat-123/Resume/blob/main/CV.pdf" target="_blank">Vuoi visionarlo?</a> Contattami a matteo.montani@proton.me se hai un'offerta di lavoro!`,
     tv: `Ecco una lista dei miei progetti:
-    <ul><li><a href="https://github.com/Mat-123/WebSitePortfolio" target="_blank">WebSite Portfolio Game</li><li><a href="https://github.com/Mat-123/CrypTracker" target="_blank">CrypTracker</a></li></ul>`,
+    <ul>
+    <li><a href="https://github.com/Mat-123/WebSitePortfolio" target="_blank">WebSite Portfolio Game</li>
+    <li><a href="https://github.com/Mat-123/CrypTracker" target="_blank">CrypTracker</a></li>
+    </ul>`,
     pic: '<figure><img src="./me.jpg" width="100%"><figcaption>Me surfin the matrix.</figcaption></figure>',
     mom: {
         text: "Ciao! Cosa stai cercando?",
