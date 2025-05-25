044e5bece4a20b89e7ab7583d2320fb295d9ce53
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2025-05-26 01:21:32 +0200

    update per deploy

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
+import dialogueDataJson from "./data/dialogue.json";
+
 export const scaleFactor = 4;
 
 // Import dialogue data from JSON file
-export const dialogueData = await (await fetch("./data/dialogue.json")).json();
+export const dialogueData = dialogueDataJson;
